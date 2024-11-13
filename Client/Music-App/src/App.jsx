ca78b3820033288a61aae890b2ec30c43a1b4ed9
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-11-13 11:24:48 +0530

    x

---
+++ Client/Music-App/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <p className="bg-black text-white">hello</p>
+      <audio controls>
+        <source src="spotify:episode:3thwnKKqM6VF9EC4E18R6T" type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
+    </>
+  );
+}
+
+export default App;
